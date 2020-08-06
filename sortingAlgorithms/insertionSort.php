f57c394f4ae295ba39b850ca29afa11dd1db1559
abdelrhman <mohamed95zoytak@gmail.com>
2020-08-06 02:53:47 +0200

    wrote bubble, insertion, merge, radix, selection sorting algorithm

---
+++ sortingAlgorithms/insertionSort.php
@@ -0,0 +1,15 @@
+<?php
+//works perfectly if your array nearly sorted
+function insertionSort(array $array){
+
+    for ($i=1;$i<count($array);$i++){
+        $currentVal=$array[$i];
+        for ($j=$i-1;$j>=0&&$array[$j]>$currentVal;$j--){
+          $array[$j+1]=$array[$j];
+        }
+        $array[$j+1]=$currentVal;
+    }
+    return $array;
+}
+
+

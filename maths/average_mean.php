169f90c5b0e3a03fad0610c41b4bf6e6b1b3dd79
psirus0588 <psirus0588@gmail.com>
2020-08-14 11:37:42 +0300

    Create average_mean.php
    Add implementation for average mean on PHP (https://github.com/TheAlgorithms/Algorithms-Explanation/blob/master/Basic%20Math/Average_Mean.md)

---
+++ maths/average_mean.php
@@ -0,0 +1,31 @@
+<?php 
+/**
+ * link: https://github.com/TheAlgorithms/Algorithms-Explanation/blob/master/Basic%20Math/Average_Mean.md
+ * title: Average (Mean)
+ * description: Calculate the average of a list of numbers using mean.
+*/
+
+
+// Step 1: Input a list of numbers.
+	echo "\n Rounded average mean: ";
+	echo fn_average_mean([2, 4, 6, 8, 20, 50, 70], true);
+	echo "\n Average mean: ";
+	echo fn_average_mean([2, 4, 6, 8, 20, 50, 70]);
+	echo "\n";
+
+
+/**
+ * Find average mean
+ *
+ * @param      array    $input_arr        Input a list of numbers.
+ * @param      boolean  $is_round_output  Indicates if round output, default false.
+ *
+ * @return     integer  Return average mean.
+ */
+function fn_average_mean(array $input_arr, bool $is_round_output = false)
+{
+
+	$output_average = array_sum($input_arr) / count($input_arr); 
+	return $is_round_output ? round($output_average) : $output_average;
+
+}

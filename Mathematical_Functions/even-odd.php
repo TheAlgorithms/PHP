4cfbec6173d339e7f5758eeefdeef53034bb120d
TinaChandwani <2017.tina.chandwani@ves.ac.in>
2020-10-02 23:34:41 +0530

    Create even-odd.php
    To check if the number is even or odd.

---
+++ Mathematical_Functions/even-odd.php
@@ -0,0 +1,16 @@
+<?php 
+// PHP code to check whether the number  
+// is Even or Odd in Normal way 
+function check($number){ 
+    if($number % 2 == 0){ 
+        echo "Even";  
+    } 
+    else{ 
+        echo "Odd"; 
+    } 
+} 
+  
+// Driver Code 
+$number = 39; 
+check($number) 
+?> 

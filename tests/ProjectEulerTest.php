570f58f168dc6966b44b9a6a73ba6c27573843c9
David Adi Nugroho <davidadi216@gmail.com>
2021-10-09 14:10:38 +0700

    Add project euler p1 and p2

--- tests/ProjectEulerTest.php
+++ tests/ProjectEulerTest.php
@@ -3,12 +3,25 @@
 use PHPUnit\Framework\TestCase;
 
 require_once __DIR__.'/../vendor/autoload.php';
+require_once __DIR__.'/../ProjectEuler/Problem1.php';
+require_once __DIR__.'/../ProjectEuler/Problem2.php';
 require_once __DIR__.'/../ProjectEuler/Problem5.php';
 require_once __DIR__.'/../ProjectEuler/Problem7.php';
 require_once __DIR__.'/../ProjectEuler/Problem9.php';
 
 class ProjectEulerTest extends TestCase
 {
+    public function testProblem1(): void
+    {
+        $this->assertSame(233168, problem1a());
+        $this->assertSame(233168, problem1b());
+    }
+
+    public function testProblem2(): void
+    {
+        $this->assertSame(4613732, problem2());
+    }
+
     public function testProblem5(): void
     {
         $this->assertSame(232792560, problem5());

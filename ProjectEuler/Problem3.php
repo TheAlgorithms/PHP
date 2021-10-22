4b41b472e03f2ec8c997bb6078517e0f317e134b
Nima HeydariNasab <nima.heydari79@yahoo.com>
2021-10-22 20:57:24 +0330

    Add Project Euler problem 3

---
+++ ProjectEuler/Problem3.php
@@ -0,0 +1,30 @@
+<?php
+
+/**
+ * Problem:
+ *
+ * The prime factors of 13195 are 5, 7, 13 and 29.
+ *
+ * What is the largest prime factor of the number 600851475143?
+ *
+ *
+ * Answer:
+ *
+ * 6857
+ *
+ *
+ * @link https://projecteuler.net/problem=3
+ */
+function problem3(): int
+{
+    $n = 600851475143;
+    $i = 2;
+    while ($i * $i < $n) {
+        while ($n % $i == 0) {
+            $n /= $i;
+        }
+        $i++;
+    }
+
+    return $n;
+}

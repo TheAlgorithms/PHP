f29d25b4321d9cff7f92aaad1e459810a75bce87
Andrew S Erwin <andrew@andrewerwin.com>
2021-11-16 16:34:52 -0600

    counts start at 1 in stead of 0. skews positions - fixed

--- tests/fibonacciSearchTest.php
+++ tests/fibonacciSearchTest.php
@@ -16,10 +16,10 @@ class fibonacciSearchTest extends TestCase
     {
         $test1 = fibonacciPosition(6); // 8
         $test2 = fibonacciPosition(9); // 34
-        $test3 = fibonacciPosition(60); // 956722026041
+        $test3 = fibonacciPosition(60); // 1548008755920
 
         $this->assertEquals(8, $test1);
         $this->assertEquals(34, $test2);
-        $this->assertEquals(956722026041, $test3);
+        $this->assertEquals(1548008755920, $test3);
     }
 }

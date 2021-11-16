888e46143ba4b9735e42988687adfb7a44944457
Andrew S Erwin <andrew@andrewerwin.com>
2021-11-16 16:26:17 -0600

    fix typo in test

--- tests/fibonacciSearchTest.php
+++ tests/fibonacciSearchTest.php
@@ -1,7 +1,7 @@
 <?php
 
-require_once __DIR__ . '/../../vendor/autoload.php';
-require_once __DIR__ . '../../searches/fibonacciSearch.php';
+require_once __DIR__ . '/../vendor/autoload.php';
+require_once __DIR__ . '/../searches/fibonacciSearch.php';
 
 use PHPUnit\Framework\TestCase;
 

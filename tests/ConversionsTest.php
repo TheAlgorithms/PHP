d20c5686d1b04514ba2a84b3f88455c6589632e9
Andrew S Erwin <andrew@andrewerwin.com>
2021-11-18 17:11:01 -0600

    mistakenly added line to ConversionsTest - fixed problem

--- tests/ConversionsTest.php
+++ tests/ConversionsTest.php
@@ -8,7 +8,6 @@ require_once __DIR__ . '/../vendor/autoload.php';
 require_once __DIR__ . '/../Conversions/BinaryToDecimal.php';
 require_once __DIR__ . '/../Conversions/OctalToDecimal.php';
 require_once __DIR__ . '/../Conversions/HexadecimalToDecimal.php';
-require_once __DIR__ . '/../Conversions/DecimalToBinary.php';
 
 class ConversionsTest extends TestCase
 {

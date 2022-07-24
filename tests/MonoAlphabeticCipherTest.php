d75ccafe73c51f8aba60ab8158abbd0bd10d3dd9
YvonneChong18 <89377606+YvonneChong18@users.noreply.github.com>
2022-07-24 13:08:15 +0800

    Update Mono-AlphabeticCipher Test
    Update Mono-AlphabeticCipher Test

--- tests/MonoAlphabeticCipherTest.php
+++ tests/MonoAlphabeticCipherTest.php
@@ -2,11 +2,11 @@
 use PHPUnit\Framework\TestCase;
 use function PHPUnit\Framework\assertEquals;
 
-require_once __DIR__ . '/../app/monoAlphabeticCipher.php';
+require_once __DIR__ . '/../ciphers/monoAlphabeticCipher.php';
 
 class MonoAlphabeticCipherTest extends TestCase
 {
-    public function testLala(){
+    public function testMonoAlphabeticCipher(){
         $alphabet = "abcdefghijklmnopqrstuvwxyz";
         $key = "yhkqgvxfoluapwmtzecjdbsnri";
         $text = "I love1234 GitHub";

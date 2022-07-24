505bee2036ffb7d4eae23073b5ec68022eff6daa
Brandon Johnson <brandon@moov.io>
2022-07-23 20:41:10 -0600

    ciphers: some style clean-up of the XORCipher

--- Ciphers/XORCipher.php
+++ Ciphers/XORCipher.php
@@ -1,17 +1,24 @@
 <?php
-// The XOR cipher is a type of additive cipher.
-// Each character is bitwise XORed with the key.
-// We loop through the input string, XORing each
-// character with the key.
-function xor_cipher(string $inp_string, string $key)
+/* The XOR cipher is a type of additive cipher.
+ * Each character is bitwise XORed with the key.
+ * We loop through the input string, XORing each
+ * character with the key.
+ * The key is repeated until it is the same length as the input.
+ *
+ * @param string $input The input string.
+ * @param string $key The key to use.
+ * @return string The encrypted string.
+ */
+ */
+function xorCipher(string $input_string, string $key)
 {
 
     $key_len = strlen($key);
     $result = array();
 
-    for ($idx = 0;$idx < strlen($inp_string);$idx++)
+    for ($idx = 0; $idx < strlen($input_string); $idx++)
     {
-        array_push($result, $inp_string[$idx] ^ $key[$idx % $key_len]);
+        array_push($result, $input_string[$idx] ^ $key[$idx % $key_len]);
     }
 
     return join("", $result);

2748ebbb2fcbde5a06623c63fdb8856f4481d042
Desmond325 <xinyu0325@gmail.com>
2022-08-06 16:09:36 +0800

    test script
    change testing code

--- tests/Sorting/CombSortTest.php
+++ tests/Sorting/CombSortTest.php
@@ -10,14 +10,14 @@ class combSortTest extends TestCase
     public function testCombSort() 
     {
         $array = array(5, 2, 1, 7, -2, 8, 4);
-        $sorted = gnomeSort($array);
+        $sorted = combSort($array);
         $this->assertEquals([-2, 1, 2, 4, 5, 7, 8], $sorted);
     }
 
     public function testCombSort2() 
     {
         $array = array(-4, -1, 7, 0, 5, 3, 1);
-        $sorted = gnomeSort($array);
+        $sorted = combSort($array);
         $this->assertEquals([-4, -1, 0, 1, 3, 5, 7], $sorted);
     }
 

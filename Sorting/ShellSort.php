e2a6529150f8de825e91a14b338daa24cf51232b
Desmond325 <87235152+Desmond325@users.noreply.github.com>
2022-08-07 01:01:51 +0800

    update ShellSort
    Add some comments on the source code.

--- Sorting/ShellSort.php
+++ Sorting/ShellSort.php
@@ -1,4 +1,14 @@
 <?php
+
+/** Shell sort is an in-place comparison sort.
+   * Starting with pairs of items that are far off from one another, the approach gradually closes the distance between the elements to be compared. 
+   * Some out-of-place components may be brought into position more quickly than with a basic closest neighbour exchange by starting with far-off elements.
+
+   * @param array $array 
+   * @param $a
+   * @param $b
+   * @return array $array*/
+
 function shellSort($array)
 {
 	$a = round(count($array)/2);

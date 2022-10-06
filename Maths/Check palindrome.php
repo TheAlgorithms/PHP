877a030d4a5c8321892edfe21c2f9c89cb86c5aa
Abdul-Samad99 <abdulsamad.ismail@systemsltd.com>
2022-10-06 20:31:24 +0500

    Added algorithm for checking palindromic numbers

---
+++ Maths/Check palindrome.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * This function checks if given number is palindromic
+ * e.g. 121
+ * @param integer $input
+ * @return boolean
+ */
+function checkPalindrome(int $input): bool
+{
+  $arr = array_map('intval', str_split($input));
+  $arrRev = array_reverse($arr);
+  $inputRev = (int)implode("", $arrRev);
+  if ($input == $inputRev) {
+    return true;
+  }
+  else {
+    return false;
+  }
+}
+?>

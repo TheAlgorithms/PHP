fd327c7527c6d41247e8893cbff2e18268d461d4
aryanshb <77982206+aryanshb@users.noreply.github.com>
2022-11-25 13:43:40 +0530

    Update DepthFirstSearchTest.php

--- tests/Graphs/DepthFirstSearchTest.php
+++ tests/Graphs/DepthFirstSearchTest.php
@@ -1,6 +1,6 @@
 <?php
 
-// require_once __DIR__ . '/../../vendor/autoload.php';
+require_once __DIR__ . '/../../vendor/autoload.php';
 require_once __DIR__ . '/../../Graphs/DepthFirstSearch.php';
 
 use PHPUnit\Framework\TestCase;

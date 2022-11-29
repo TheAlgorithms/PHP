1c33b1f1dd4282642f7a877ee63197a260e62330
Aryansh B <aryanshbhargavan@outlook.com>
2022-11-29 12:51:02 +0530

    Added BFS (#98)
    * added BFS
    
    * Added tests, updated directory, added BFS
    
    * modified test
    
    * Update DepthFirstSearchTest.php
    
    * Modified requested change

---
+++ tests/Graphs/BreadthFirstSearchTest.php
@@ -0,0 +1,49 @@
+<?php
+
+require_once __DIR__ . '/../../vendor/autoload.php';
+require_once __DIR__ . '/../../Graphs/BreadthFirstSearch.php';
+
+use PHPUnit\Framework\TestCase;
+
+class BreadthFirstSearchTest extends TestCase
+{
+    public function testBreadthFirstSearch()
+    {
+        $graph = array(
+            "A" => ["B", "C", "D"],
+            "B" => ["A", "D", "E"],
+            "C" => ["A", "F"],
+            "D" => ["B", "D"],
+            "E" => ["B", "F"],
+            "F" => ["C", "E", "G"],
+            "G" => ["F"],
+        );
+        $ans = bfs($graph, "A", "F");
+        $this->assertTrue($ans);
+    }
+    public function testBreadthFirstSearch2()
+    {
+        $graph = array(
+            [1, 2],
+            [2],
+            [0, 3],
+            [3],
+        );
+
+        $ans2 = bfs($graph, 3, 0);
+        $this->assertFalse($ans2);
+    }
+    public function testBreadthFirstSearch3()
+    {
+        $graph = array(
+            [2, 3, 1],
+            [0],
+            [0, 4],
+            [0],
+            [2],
+        );
+        $ans = bfs($graph, 0, 3);
+        $this->assertTrue($ans);
+    }
+}
+

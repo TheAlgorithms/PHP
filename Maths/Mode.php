50f436eb1793aca6380e19af88038e5cdf01d56d
salehhashemi1992 <81674631+salehhashemi1992@users.noreply.github.com>
2023-09-16 16:45:18 +0330

    fix mode method to return multiple modes if needed

--- Maths/Mode.php
+++ Maths/Mode.php
@@ -1,20 +1,21 @@
 <?php
 /**
  * This function calculates
- * The mode value of
+ * the mode value(s) of
  * numbers provided
- * 
+ *
  * @param decimal $numbers A variable sized number input
- * @return decimal $mode Mode of provided numbers
+ * @return array $modes Array of mode(s) of provided numbers
+ * @throws \Exception
  */
 function mode(...$numbers)
 {
     if (empty($numbers)) {
-        throw new \Exception('Please pass values to find mean value');
+        throw new \Exception('Please pass values to find the mode');
     }
 
-    $values = array_count_values($numbers); 
-    $mode = array_search(max($values), $values);
+    $values = array_count_values($numbers);
+    $maxCount = max($values);
 
-    return $mode;
+    return array_keys($values, $maxCount, true);
 }

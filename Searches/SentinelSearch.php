7cd384f2d2b1f0e752ae8f3cbe7e033b931e7edd
Abhishek Pashte <90621734+Abhishek-Pashte@users.noreply.github.com>
2023-10-06 12:24:21 +0530

    Create SentinelSearch.php
    Added Sentinel Search algorithm in Searches folder

---
+++ Searches/SentinelSearch.php
@@ -0,0 +1,52 @@
+<?php
+/* SentinelSearch 
+    Input : - 
+    parameter 1: Array
+    parameter 2: Target element
+
+    Output : - 
+    Returns index of element if found, else -1
+*/
+function SentinelSearch($list, $target){
+
+    //Length of array
+    $len=sizeof($list);
+
+    //Store last element of array
+    $lastElement=$list[$len-1];
+
+    //Put target at the last position of array known as 'Sentinel'
+    if($lastElement == $target){
+        return ($len-1);
+    }
+
+    //Put target at last index of array
+    $list[$len-1]=$target;
+
+    //Initialize variable to traverse through aaray
+    $i=0;
+
+    //Traverse through array to search target
+    while($list[$i]!=$target){
+        $i++;
+    }
+
+    //Put last element at it's position 
+    $list[$len-1]=$lastElement;
+
+    //If i in less than length, It means element is present in array
+    if (($i < $len - 1))
+        return $i;
+    // Else element is not present return -1
+    else
+        return -1;
+}
+$arr=array(1,2,3,4,5,6);
+$key=6;
+$index=SentinelSearch($arr,$key);
+if($index!=-1){
+    echo "$key found at index $index";
+}
+else 
+    echo "$key not found";
+?>

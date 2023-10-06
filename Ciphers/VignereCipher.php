240260b21bbfb70f62e8a30a58763e98c2960932
Aryansh Bhargavan <aryanshbhargavan@outlook.com>
2023-10-06 21:48:11 +0530

    Fix: Changes in formatting?

--- Ciphers/VignereCipher.php
+++ Ciphers/VignereCipher.php
@@ -1,22 +1,27 @@
 <?php
-// Function to encrypt plaintext using Vigenère cipher
-function vigenere_encrypt($plaintext, $key) {
+
+/**
+ * Encrypts a plaintext using the Vigenère cipher.
+ * (https://en.wikipedia.org/wiki/Vigen%C3%A8re_cipher)
+ *
+ * @param string $plaintext The plaintext to be encrypted.
+ * @param string $key The encryption key.
+ * @return string The encrypted text.
+ */
+function vigenere_encrypt($plaintext, $key)
+{
     // Convert the input to uppercase for consistency
     $plaintext = strtoupper($plaintext);
     $key = strtoupper($key);
-    
     $keyLength = strlen($key);
     $encryptedText = "";
-
     for ($i = 0; $i < strlen($plaintext); $i++) {
         $char = $plaintext[$i];
         if (ctype_alpha($char)) {
             // Calculate the shift based on the key
             $shift = ord($key[$i % $keyLength]) - ord('A');
-            
             // Apply the Vigenère encryption formula
             $encryptedChar = chr(((ord($char) - ord('A') + $shift) % 26) + ord('A'));
-            
             // Append the encrypted character to the result
             $encryptedText .= $encryptedChar;
         } else {
@@ -24,26 +29,29 @@ function vigenere_encrypt($plaintext, $key) {
             $encryptedText .= $char;
         }
     }
-
     return $encryptedText;
 }
 
-// Function to decrypt ciphertext using Vigenère cipher
-function vigenere_decrypt($ciphertext, $key) {
+/**
+ * Decrypts a ciphertext using the Vigenère cipher.
+ *
+ * @param string $ciphertext The ciphertext to be decrypted.
+ * @param string $key The decryption key.
+ * @return string The decrypted text.
+ */
+function vigenere_decrypt($ciphertext, $key)
+{
     $ciphertext = strtoupper($ciphertext);
     $key = strtoupper($key);
     $keyLength = strlen($key);
     $decryptedText = "";
-
-    for ($i = 0; $i < strlen($ciphertext); $i++) {
+    for ($i = 0; $i < strlen($ciphertext); $i++){
         $char = $ciphertext[$i];
         if (ctype_alpha($char)) {
             // Calculate the shift based on the key
             $shift = ord($key[$i % $keyLength]) - ord('A');
-            
             // Apply the Vigenère decryption formula
             $decryptedChar = chr(((ord($char) - ord('A') - $shift + 26) % 26) + ord('A'));
-            
             // Append the decrypted character to the result
             $decryptedText .= $decryptedChar;
         } else {
@@ -51,7 +59,5 @@ function vigenere_decrypt($ciphertext, $key) {
             $decryptedText .= $char;
         }
     }
-
     return $decryptedText;
 }
-?>

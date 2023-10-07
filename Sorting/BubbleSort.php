536f2c407ba1d0c226aefc2a3a6c1bba25821543
Prathamesh Parulekar <phparulekar@gmail.com>
2023-10-07 20:56:34 +0530

    added case for check Anagram

--- Sorting/BubbleSort.php
+++ Sorting/BubbleSort.php
@@ -11,18 +11,18 @@ function bubbleSort($array)
     $length = count($array);
 
     for ($i = $length; $i > 0; $i--) {
-        $swapped = true;
+        $swapped = false;
 
         for ($j = 0; $j < $i - 1; $j++) {
             if ($array[$j] > $array[$j + 1]) {
                 $temp = $array[$j];
                 $array[$j] = $array[$j + 1];
                 $array[$j + 1] = $temp;
-                $swapped = false;
+                $swapped = true;
             }
         }
 
-        if ($swapped) {
+        if (!$swapped) {
             break;
         }
     }
--- Strings/CheckAnagram.php
+++ Strings/CheckAnagram.php
@@ -11,11 +11,13 @@
 function isAnagram(string $originalString, string $testString, bool $caseInsensitive = true)
 {
     if ($caseInsensitive) {
-        $originalString = strtolower($originalString); // Converting string to lowercase for case-insensitive check
+        $originalString = strtolower($originalString);
         $testString = strtolower($testString);
     }
 
-    // count_chars(string, mode = 1) returns key-value pairs with character as key, frequency as value
-    // We can directly compare the arrays in this case
+    if (strlen($originalString) !== strlen($testString)) {
+        return false; 
+    }
+
     return count_chars($originalString, 1) === count_chars($testString, 1);
 }

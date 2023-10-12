85cffbd9f4bd445861fe1551f0074410487e26fd
Abhishek Pashte <90621734+Abhishek-Pashte@users.noreply.github.com>
2023-10-12 11:26:49 +0530

    Updated SentinelSearch.php

--- Searches/SentinelSearch.php
+++ Searches/SentinelSearch.php
@@ -1,45 +1,46 @@
 <?php
-/* SentinelSearch 
-    Input : - 
+
+/* SentinelSearch
+    Input : -
     parameter 1: Array
     parameter 2: Target element
 
-    Output : - 
+    Output : -
     Returns index of element if found, else -1
 */
-function SentinelSearch($list, $target){
-
+function SentinelSearch($list, $target)
+{
     //Length of array
-    $len=sizeof($list);
+    $len = sizeof($list);
 
     //Store last element of array
-    $lastElement=$list[$len-1];
+    $lastElement = $list[$len-1];
 
     //Put target at the last position of array known as 'Sentinel'
-    if($lastElement == $target){
-        return ($len-1);
-    }
-
+    if ($lastElement == $target)
+    {
+        return ($len - 1);
+    } 
+    
     //Put target at last index of array
-    $list[$len-1]=$target;
+    $list[$len - 1] = $target;
 
-    //Initialize variable to traverse through aaray
+    //Initialize variable to traverse through array
     $i=0;
 
     //Traverse through array to search target
-    while($list[$i]!=$target){
+    while ($list[$i] != $target)
+    {
         $i++;
-    }
-
-    //Put last element at it's position 
-    $list[$len-1]=$lastElement;
+    } 
+    //Put last element at it's position
+    $list[$len - 1] = $lastElement;
 
     //If i in less than length, It means element is present in array
-    if (($i < $len - 1))
+    if ($i < ($len - 1))
         return $i;
     // Else element is not present return -1
-    else
+    else 
         return -1;
 }
-
 ?>

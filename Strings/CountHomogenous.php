226fd5488c57e052faa1fb98809965759e2a1e27
niharikamahajan02 <72392580+niharikamahajan02@users.noreply.github.com>
2024-01-08 22:20:02 +0530

    Update Strings/CountHomogenous.php
    Co-authored-by: Brandon Johnson <bbj1979@gmail.com>

--- Strings/CountHomogenous.php
+++ Strings/CountHomogenous.php
@@ -1,25 +1,25 @@
 <?php
 
-    /**
-     * Count homogenous substrings 
-     * @param String $s
-     * @return Integer
-     */
-    function countHomogenous($s)
-    {
-        $mod = 1000000007;
-        $finalcnt = 0;
-        $len = 1;
+/**
+ * Count homogenous substrings
+ * @param String $s
+ * @return Integer
+ */
+function countHomogenous($s)
+{
+    $mod = 1000000007;
+    $finalcnt = 0;
+    $len = 1;
 
-        for ($i = 1; $i <= strlen($s); $i++) {
-            if ($i < strlen($s) && $s[$i] == $s[$i - 1]) {
-                $len++;
-            } else {
-                // calculating the sum of first n(len) natural no. and finally adding it to our final answer.
-                $finalcnt = ($finalcnt + ($len * ($len+ 1) / 2)) % $mod;
-                $len = 1;  // Now count new substring
-            }
+    for ($i = 1; $i <= strlen($s); $i++) {
+        if ($i < strlen($s) && $s[$i] == $s[$i - 1]) {
+            $len++;
+        } else {
+            // calculating the sum of first n(len) natural no. and finally adding it to our final answer.
+            $finalcnt = ($finalcnt + ($len * ($len + 1) / 2)) % $mod;
+            $len = 1;  // Now count new substring
         }
-
-        return $finalcnt;
     }
+
+    return $finalcnt;
+}

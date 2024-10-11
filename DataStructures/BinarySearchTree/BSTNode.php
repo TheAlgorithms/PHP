f546cb39ed3a86492f0d2a2cc7034186710e0d62
Ramy-Badr-Ahmed <ramy.badr@hotmail.com>
2024-10-11 12:41:25 +0200

    Implementing Binary Search Tree as Data Structure

---
+++ DataStructures/BinarySearchTree/BSTNode.php
@@ -0,0 +1,14 @@
+<?php
+
+/*
+ * Created by: Ramy-Badr-Ahmed (https://github.com/Ramy-Badr-Ahmed) in Pull Request: #173
+ * https://github.com/TheAlgorithms/PHP/pull/173
+ *
+ * Please mention me (@Ramy-Badr-Ahmed) in any issue or pull request addressing bugs/corrections to this file.
+ * Thank you!
+ */
+
+class BSTNode
+{
+
+}
---
+++ tests/DataStructures/BSTreeTest.php
@@ -0,0 +1,22 @@
+<?php
+
+/*
+ * Created by: Ramy-Badr-Ahmed (https://github.com/Ramy-Badr-Ahmed) in Pull Request: #173
+ * https://github.com/TheAlgorithms/PHP/pull/173
+ *
+ * Please mention me (@Ramy-Badr-Ahmed) in any issue or pull request addressing bugs/corrections to this file.
+ * Thank you!
+ */
+
+namespace DataStructures;
+
+require_once __DIR__ . '/../../vendor/autoload.php';
+require_once __DIR__ . '/../../DataStructures/BinarySearchTree/BSTree.php';
+require_once __DIR__ . '/../../DataStructures/BinarySearchTree/BSTNode.php';
+
+use PHPUnit\Framework\TestCase;
+
+class BSTreeTest extends TestCase
+{
+
+}
